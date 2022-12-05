fdf50056ad75063fc902af80d51b3f88ea55f22d
greyqwilson <98433648+greyqwilson@users.noreply.github.com>
2022-12-04 16:01:00 -0800

    fixed assignment issue (now blocking)
    fixed issue of mem_out being off by a cycle causing major issues

--- DataMemory.v
+++ DataMemory.v
@@ -5,13 +5,13 @@ input [31:0] write_data; //Pairs with mem_write
 input read_enable; //Can only read or write at any given moment. Not both at once
 input write_enable;
 input clk;
-output reg [31:0] read_data; //Hates to compile here
-// output wire [31:0] read_data;
+//output reg [31:0] read_data; //Hates to compile here
+output wire [31:0] read_data;
 
 reg [7:0] memArr [31:0]; //of memory
 
 initial begin
-    $readmemh("data_memory.mem", memArr);
+    $readmemh("data_memory2.mem", memArr);
 end
 
 always @(posedge clk)
@@ -22,11 +22,11 @@ begin
     memArr[address+3] <= write_enable ? write_data[31:24] : memArr[address+3];    
 
     
-    read_data <= read_enable ? {memArr[address+3],memArr[address+2], memArr[address+1],memArr[address]} 
-                             : {32{read_enable}}; //ElectroBinary has these assigned as bytes, not words.
+    // read_data <= read_enable ? {memArr[address+3],memArr[address+2], memArr[address+1],memArr[address]} 
+    //                          : {32{read_enable}}; //ElectroBinary has these assigned as bytes, not words.
 end
 
-// assign read_data = {memArr[address+3],memArr[address+2],
-//                         memArr[address+1],memArr[address]}; //ElectroBinary has these assigned as bytes, not words.
+assign read_data = {memArr[address+3],memArr[address+2],
+                        memArr[address+1],memArr[address]}; //ElectroBinary has these assigned as bytes, not words.
 
 endmodule
